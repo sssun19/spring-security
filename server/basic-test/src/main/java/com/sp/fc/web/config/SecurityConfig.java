1fcd543bf1084561c2e59e221f153914e4228ddc
sssun <98_0509@naver.com>
2024-10-22 22:37:26 +0900

    login-basic

--- server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,50 +1,62 @@
 package com.sp.fc.web.config;
 
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity(debug = true) //시큐리티 필터 체인의 종류를 디버그로 찍을 수 있음
+@EnableGlobalMethodSecurity(prePostEnabled = true) //prePost 로 권한 체크 선언
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    UserDetailsService userService(){
-        final PasswordEncoder pw = passwordEncoder();
-        UserDetails user2 = User.builder()
-                .username("user2")
-                .password(pw.encode("1234"))
-                .roles("USER").build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(pw.encode("1234"))
-                .roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user2, admin);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.builder()
+                        .username("user2")
+                        .password(passwordEncoder().encode("2222"))
+                        .roles("USER"))
+
+                .withUser(User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("3333"))
+                        .roles("ADMIN"));
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                .and()
-                .authorizeRequests(auth->{
-                    auth.anyRequest().permitAll()
-                            ;
-                })
-                ;
+
+        http.antMatcher("/**"); //모든 request 에 대해 필터 체인 동작
+        http.antMatcher("/api/**"); //api 하위 request 에 대해 필터 체인 동작
+
+        /** 두 개 이상의 필터 체인을 구성하려면 Security filter chain 을 하나 더 생성함. (예시로 SecurityConfig 클래스 추가 생성)
+         * 그런 경우 어떤 필터 체인 클래스를 우선 순위로 할지 @Order(n) 어노테이션으로 필수 설정해 주어야 함.
+         * 낮은 n 순서부터 먼저 체크
+         */
+
+
+        http.authorizeRequests((requests) -> {
+            requests.antMatchers("/").permitAll() //기본 페이지는 모두 접근 가능하도록
+                    .anyRequest().authenticated(); //모든 페이지를 인증 받은 후(로그인) 접근할 수 있도록
+        });
+        http.formLogin();
+        http.httpBasic();
+
+//        http.headers().disable()
+//                .csrf().disable()
+//                .formLogin(login -> login.defaultSuccessUrl("/", false))
+//                .logout().disable()
+//                .requestCache().disable();
     }
 }
--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,50 @@
 package com.sp.fc.web.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                ).withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("admin")
+                                .password("2222")
+                                .roles("ADMIN")
+                );
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(request->{
-                    request
-                            .antMatchers("/**").permitAll()
-                            ;
+        http.authorizeRequests(request->{
+                    request.antMatchers("/").permitAll()
+                            .anyRequest().authenticated();
+                    /**
+                     * 이렇게 루트 페이지만 접근 허용하고 다른 request 접근을 막아버리면
+                     * 페이지들에 대해 보안을 걸었기 때문에 CSS 가 제대로 내려가지 않는 경우가 발생한다.
+                     * 웹 리소스(css,...)에 대해 security filter 를 적용되지 않도록 ignore 시켜야 함.
+                     */
                 })
-                ;
+                .formLogin(login->login.loginPage("/login").permitAll());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()); // image, css, js bootstrap
     }
 }
