b5cd72f401bb150b98722f457dd5a20a6c0a1a7e
sssun <98_0509@naver.com>
2024-10-31 21:28:58 +0900

    POSTë° 방식 테스트

--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -3,6 +3,7 @@ package com.sp.fc.web;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -47,12 +48,32 @@ public class BasicAuthenticationTest {
         headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
                 "user1:1111".getBytes()
         ));
+
         HttpEntity entity = new HttpEntity(null, headers);
         ResponseEntity<String> response = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);
-
         assertEquals("hello", response.getBody());
 
         System.out.println(response.getBody());
     }
-    
+
+    @DisplayName("3. 인증성공2")
+    @Test
+    void test3() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String resp = testClient.getForObject(greetingUrl(), String.class);
+        assertEquals("hello", resp);
+    }
+
+    /**
+     * POST 방식은 csrf 보안이 작동하고 있어서 Config 클래스에서 csrf().disable() 해주어야 테스트 가능.
+     */
+    @DisplayName("4. POST 인증")
+    @Test
+    void test4() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> resp = testClient.postForEntity(greetingUrl(), "kimsuna", String.class);
+        assertEquals("hello kimsuna", resp.getBody());
+    }
+
+
 }
