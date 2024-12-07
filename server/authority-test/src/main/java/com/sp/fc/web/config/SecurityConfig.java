1a3cf313319f32823636103f8c10d4d2123b3c52
sssun <98_0509@naver.com>
2024-12-07 18:51:34 +0900

    임커스텀 AccessDecisionManager 올리기

---
+++ server/authority-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.sp.fc.web.config;
+
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterInvocation;
+
+import java.util.Collection;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    AccessDecisionManager filterAccessDecisionManager() {
+        return new AccessDecisionManager() {
+
+            @Override
+            public void decide(Authentication authentication, Object o, Collection<ConfigAttribute> collection) throws AccessDeniedException, InsufficientAuthenticationException {
+//                throw new AccessDeniedException("접근 금지");
+                return;
+            }
+
+            @Override
+            public boolean supports(ConfigAttribute configAttribute) {
+                return true;
+            }
+
+            @Override
+            public boolean supports(Class<?> clazz) {
+                return FilterInvocation.class.isAssignableFrom(clazz);
+            }
+
+        };
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests(authority -> authority
+                        .mvcMatchers("/greeting").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                        .accessDecisionManager(filterAccessDecisionManager())
+
+                )
+        ;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                );
+    }
+}
---
+++ server/authority-test/src/test/java/com/sp/fc/web/test/AuthorityBasicTest.java
@@ -0,0 +1,27 @@
+package com.sp.fc.web.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuthorityBasicTest extends WebIntegrationTest{
+
+
+    TestRestTemplate client;
+
+    @DisplayName("1. greeting 메세지를 불러온다.")
+    @Test
+    void test_1() {
+        client = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> response = client.getForEntity(uri("/greeting"), String.class);
+
+        assertEquals("hello", response.getBody());
+        System.out.println(response.getBody());
+    }
+
+}
