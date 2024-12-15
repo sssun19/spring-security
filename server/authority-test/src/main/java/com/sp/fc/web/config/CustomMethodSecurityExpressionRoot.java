71b442e3a8b108a98f57cb6d9f8f125cd69a2f86
sssun <98_0509@naver.com>
2024-12-15 22:42:06 +0900

    CustomSecurityExpressiongRoot

---
+++ server/authority-test/src/main/java/com/sp/fc/web/config/CustomMethodSecurityExpressionRoot.java
@@ -0,0 +1,47 @@
+package com.sp.fc.web.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionOperations;
+import org.springframework.security.core.Authentication;
+
+import java.lang.reflect.Method;
+
+@Getter
+@Setter
+public class CustomMethodSecurityExpressionRoot extends SecurityExpressionRoot
+        implements MethodSecurityExpressionOperations {
+
+    MethodInvocation invocation;
+    public CustomMethodSecurityExpressionRoot(Authentication authentication, MethodInvocation invocation) {
+        super(authentication);
+        this.invocation = invocation;
+    }
+
+    private Object filterObject;
+    private Object returnObject;
+
+    public boolean isStudent() {
+        return getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_STUDENT"));
+
+        /*
+        .filter(a -> a.getAuthority().equals("ROLE_STUDENT"))
+                .findAny().isPresent(); 를 .anyMatch() 메서드로
+         */
+    }
+
+    public boolean isTutor() {
+        return getAuthentication().getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_TUTOR"));
+
+    }
+
+
+    @Override
+    public Object getThis() {
+        return this;
+    }
+}
